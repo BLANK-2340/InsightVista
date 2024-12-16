12c840fe28d222a0cb80994efbca4a47ca3451eb
Armaan singh <armaan50069@gmail.com>
2024-12-16 10:31:30 +0530

    Create server.js

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/db');
+
+connectDB();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
