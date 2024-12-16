503132fc290eafd77ee64f54d843b59a1a04245e
Armaan singh <armaan50069@gmail.com>
2024-12-16 10:43:40 +0530

    Create NotFoundPage.js

---
+++ Frontend/src/pages/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl">404 - Page Not Found</h1>
+    </div>
+  );
+}
